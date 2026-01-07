pbatard/Mosby

commit 864d0c181b60b2b1856cbe6b072a8dea7fa652d5

Fix Security Violation when providing an external PK
* PKs should obviously not have the APPEND attribute, yet we defaulted to adding it in pki.c - Fix that.

src/pki.c
@@ -428,8 +428,17 @@ EFI_STATUS PopulateAuthVar(
 		ReportErrorAndExit(L"'%s' is too small to be a valid certificate or signature list\n", Entry->Path);
 
 	// Set default attributes for authenticated variable
-	if (Entry->Attrs == 0)
-		Entry->Attrs = (Entry->Type == MOK) ? UEFI_VAR_NV_BS_AP : UEFI_VAR_NV_BS_RT_AT_AP;
+	switch(Entry->Type) {
+		case MOK:
+			Entry->Attrs = UEFI_VAR_NV_BS_AP;
+			break;
+		case PK:
+			Entry->Attrs = UEFI_VAR_NV_BS_RT_AT;
+			break;
+		default:
+			Entry->Attrs = UEFI_VAR_NV_BS_RT_AT_AP;
+			break;
+	}
 
 	// Check for signed ESL (PKCS#7 only)
 	AuthVar = (EFI_VARIABLE_AUTHENTICATION_2*)Entry->Buffer.Data;

commit 2a60d8e0b4069b1a7fcd6e71b0de9ae907029367

Fix regression with Merged KEK attributes being overridden
* This fixes a regression introduced in 864d0c181b60b2b1856cbe6b072a8dea7fa652d5 where, when we added the switch(),
  we unintentionally dropped the very much needed if (Entry->Attrs == 0) guard.
* This issue caused platforms, such as the ASUS X99 Deluxe II, to reject the KEK update with 'Invalid Parameter'.
* Also "One does not simply..."

src/pki.c
@@ -394,7 +394,7 @@ EFI_STATUS CreateEmptyAuthVar(
 	if (Variable->Data == NULL)
 		ReportErrorAndExit(L"Failed to create empty variable: %r\n", Status);	
 
-	// Don't ask me how I found that one can simply cannot use mTime here...
+	// Don't ask me how I found that one does not simply use mTime here...
 	Status = gRT->GetTime(&Variable->Data->TimeStamp, NULL);
 	if (EFI_ERROR(Status))
 		ReportErrorAndExit(L"Failed to get current time: %r\n", Status);	
@@ -436,16 +436,18 @@ EFI_STATUS PopulateAuthVar(
 		ReportErrorAndExit(L"'%s' is too small to be a valid certificate or signature list\n", Entry->Path);
 
 	// Set default attributes for authenticated variable
-	switch(Entry->Type) {
-		case MOK:
-			Entry->Attrs = UEFI_VAR_NV_BS_AP;
-			break;
-		case PK:
-			Entry->Attrs = UEFI_VAR_NV_BS_RT_AT;
-			break;
-		default:
-			Entry->Attrs = UEFI_VAR_NV_BS_RT_AT_AP;
-			break;
+	if (Entry->Attrs == 0) {
+		switch(Entry->Type) {
+			case MOK:
+				Entry->Attrs = UEFI_VAR_NV_BS_AP;
+				break;
+			case PK:
+				Entry->Attrs = UEFI_VAR_NV_BS_RT_AT;
+				break;
+			default:
+				Entry->Attrs = UEFI_VAR_NV_BS_RT_AT_AP;
+				break;
+		}
 	}
 
 	// Check for signed ESL (PKCS#7 only)